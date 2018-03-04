5e115fb4aec298a8f370c4247212f45f50660610
ultravibez <ultravibez@hotmail.com>
2018-03-04 13:14:44 +0200

    Add functionality to access the model and retrieve the flower name.

--- NameThisFlower/ViewController.swift
+++ NameThisFlower/ViewController.swift
@@ -7,19 +7,69 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
+    let imagePicker : UIImagePickerController = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        imagePicker.delegate = self
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+            //imagePicker.allowsEditing = true
+        } else {
+            imagePicker.sourceType = .savedPhotosAlbum
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let userPickedImage : UIImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            imageView.image = userPickedImage
+            
+            guard let ciImage : CIImage = CIImage(image: userPickedImage) else { fatalError("Cannot convert UIImage to CIImage") }
+            
+            detect(image: ciImage)
+            
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
     }
-
-
+    
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else { fatalError("Loading CoreML Model Failed") }
+        
+        let request : VNCoreMLRequest = VNCoreMLRequest(model: model) { (request, error) in
+            
+            guard let firstResult = request.results?.first as? VNClassificationObservation else { fatalError("Model Failed Classifying image") }
+            
+            self.navigationItem.title = firstResult.identifier
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+        
+    }
+    
+    
+    @IBAction func cameraPressed(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
 }
-
